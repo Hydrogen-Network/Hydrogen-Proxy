0829359468215b62c0bc1166c5d1d71787b34b9b
Neealdon3 <138321129+neealdon3@users.noreply.github.com>
2023-09-17 14:46:15 -0700

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-    prefix: "/uv/service/",
+    prefix: "/uv/service/hvtrs8%2F-",
     bare: "/bare/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
